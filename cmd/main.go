f1707b774dda8c66ca2b4ce028095fa08910a392
ghizdavur <ghizdavu.r@gmail.com>
2023-12-09 22:06:29 +0200

    test

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("HELLO WORLD")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, nil)
+	}
+	http.HandleFunc("/", h1)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
